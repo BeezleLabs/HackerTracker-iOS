f21dd3e36ff64a71b54810d40e56d72978594216
imachumphries <imachumphries@me.com>
2017-07-13 02:41:58 -0400

    Refactor animation filters

--- hackertracker/Animation.swift
+++ hackertracker/Animation.swift
@@ -12,6 +12,7 @@ class Animation {
 
     let pixelScaleFactor = 10.0
     let startingPixelScale = 3.0
+    let context = CIContext(options: nil)
 
     var originalSplashImage: UIImage!
     var transitionStartTime = CACurrentMediaTime()
@@ -21,6 +22,7 @@ class Animation {
     private var image: UIImage {
         didSet {
             self.onImageUpdate(image)
+            coreImage = CIImage(image: image)?.clampingToExtent()
         }
     }
 
@@ -62,14 +64,14 @@ class Animation {
 
         let progress = min((CACurrentMediaTime() - transitionStartTime) / duration, 1.0)
 
-        if let rippleImage = applyRippleFilter(progress: progress, extent: extent) {
-            image = rippleImage
-            coreImage = CIImage(image: image)?.clampingToExtent()
-            pixelFilter?.setValue(self.coreImage, forKey: kCIInputImageKey)
+        if let rippleImage = applyRippleFilter(progress: progress, extent: extent),
+            let cgImage = context.createCGImage(rippleImage, from: extent) {
+            image = UIImage(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
         }
 
-        if let pixelImage = applyPixelFilter(progress: progress, extent: extent) {
-            image = pixelImage
+        if let pixelImage = applyPixelFilter(progress: progress, extent: extent),
+            let cgImage = context.createCGImage(pixelImage, from: extent) {
+            image = UIImage(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
         }
 
         if progress >= 1.0 {
@@ -78,42 +80,26 @@ class Animation {
         }
     }
 
-    private func applyPixelFilter(progress: Double, extent: CGRect) -> UIImage? {
+    private func applyPixelFilter(progress: Double, extent: CGRect) -> CIImage? {
         guard let pixelFilter = pixelFilter else {
             print ("PixelFilter is nil")
             return nil
         }
 
-        var result: UIImage? = nil
-
         pixelFilter.setValue((pixelScaleFactor * progress) + startingPixelScale, forKey: kCIInputScaleKey)
+        pixelFilter.setValue(self.coreImage, forKey: kCIInputImageKey)
 
-        let context = CIContext(options: nil)
-        if let output = pixelFilter.outputImage?.clampingToExtent(),
-            let cgImage = context.createCGImage(output, from: extent) {
-            result = UIImage(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
-        }
-
-        return result
+        return pixelFilter.outputImage?.clampingToExtent()
     }
 
-    private func applyRippleFilter(progress: Double, extent: CGRect) -> UIImage? {
+    private func applyRippleFilter(progress: Double, extent: CGRect) -> CIImage? {
         guard let rippleFilter = rippleFilter else {
             print ("rippleFilter is nil")
             return nil
         }
 
-        var result: UIImage? = nil
-
         rippleFilter.setValue(progress, forKey: kCIInputTimeKey)
-
-        let context = CIContext(options: nil)
-        if let output = rippleFilter.outputImage?.clampingToExtent(),
-            let cgImage = context.createCGImage(output, from: extent) {
-            result = UIImage(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
-        }
-
-        return result
+        return rippleFilter.outputImage?.clampingToExtent()
     }
 
     lazy var pixelFilter: CIFilter? = {
