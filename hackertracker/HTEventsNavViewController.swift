a2d7d443b82fb04f6dd87c3eac9cb5003511c2ef
Chris Mays <chrsmys@gmail.com>
2017-07-15 14:02:33 -0400

    Fix a bug where the image nav bar sizing was incorrect.

--- hackertracker/HTEventsNavViewController.swift
+++ hackertracker/HTEventsNavViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class HTEventsNavViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationBar.setBackgroundImage(#imageLiteral(resourceName: "nav-header"), for: .default)
+        
+        let image = #imageLiteral(resourceName: "dc-25-wallpaper-blurred")
+        
+        var  transformScale : CGFloat = 1.0
+        
+        if let windowSize = UIApplication.shared.keyWindow?.frame.size.width {
+            transformScale = windowSize/image.size.width
+        }
+        
+        let size = image.size.applying(CGAffineTransform(scaleX: transformScale, y: transformScale))
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width:size.width, height: 64), true, 0.0)
+        image.draw(in: CGRect(origin: CGPoint.zero, size: size))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.navigationBar.setBackgroundImage(scaledImage, for: .default)
     }
 
 }
