24ba7cacf29d608ac95ec5cef2c9c7a4486cf2f9
imachumphries <imachumphries@me.com>
2017-07-18 00:12:52 -0400

    Schedule table: fix headers offset after event detail

--- hackertracker/HTScheduleTableViewController.swift
+++ hackertracker/HTScheduleTableViewController.swift
@@ -38,18 +38,21 @@ class BaseScheduleTableViewController: UITableViewController, EventDetailDelegat
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         if isViewLoaded {
             reloadEvents()
             tableView.scrollToNearestSelectedRow(at: UITableViewScrollPosition.middle, animated: false)
+            // Fix misplaced section header.
+            // https://stackoverflow.com/questions/33989551/misplaced-or-hidden-uitableview-section-header-while-switching-from-one-datasour
+            tableView.setContentOffset(.zero, animated: false)
+            tableView.layoutIfNeeded()
         }
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidLoad()
-        tableView.layoutIfNeeded()
         tableView.scrollToNearestSelectedRow(at: UITableViewScrollPosition.middle, animated: false)
-        
+        tableView.layoutIfNeeded()
     }
     
     func reloadEvents() {
