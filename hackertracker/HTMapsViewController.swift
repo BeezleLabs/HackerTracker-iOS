b865b3180acfae4488bbd8729085f4f41b33d0d1
Chris Mays <chrsmys@gmail.com>
2017-08-28 23:36:00 -0400

    Add location info to toorcon events.

--- hackertracker/HTMapsViewController.swift
+++ hackertracker/HTMapsViewController.swift
@@ -10,12 +10,8 @@ import UIKit
 
 class HTMapsViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var dayView: UIWebView!
-    @IBOutlet weak var nightView: UIWebView!
-    @IBOutlet weak var dayTimeSwitch: UISegmentedControl!
     
     var dayMapView : ReaderContentView?
-    var nightMapView : ReaderContentView?
 
     var roomDimensions : CGRect?
     var timeOfDay : TimeOfDay?
@@ -23,57 +19,14 @@ class HTMapsViewController: UIViewController, UIScrollViewDelegate {
     var mapLocation : Location = .unknown {
         didSet {
             switch mapLocation {
-            case .track_101, .track1_101:
-                roomDimensions = CGRect(x: 853, y: 767, width: 116, height: 222)
+            case .track1:
+                roomDimensions = CGRect(x: 107.47, y: 197.77, width: 98.09, height: 90.5)
                 break
-            case .track2, .track2_101:
-                roomDimensions = CGRect(x: 787, y: 255, width: 135, height: 235)
+            case .track2:
+                roomDimensions = CGRect(x: 295.01, y: 188.37, width: 86.2, height: 73.48)
                 break
-            case .track3:
-                roomDimensions = CGRect(x: 212, y: 332, width: 112, height: 187)
-                break
-            case .track4:
-                roomDimensions = CGRect(x: 315, y: 332, width: 90, height: 187)
-                break
-            case .capri:
-                roomDimensions = CGRect(x: 483, y: 947, width: 38, height: 49)
-                break
-            case .modena:
-                roomDimensions = CGRect(x: 530, y: 688, width: 38, height: 36)
-                break
-            case .trevi:
-                roomDimensions = CGRect(x: 532, y: 604, width: 44, height: 35)
-                break
-            case .bioHackingVillage:
-                roomDimensions = CGRect(x: 486, y: 573, width: 30, height: 65)
-                break
-            case .cryptoAndPrivacyVillage:
-                roomDimensions = CGRect(x: 627, y: 933, width: 57, height: 61)
-                break
-            case .hardwareHackingVillage:
-                roomDimensions = CGRect(x: 231.5, y: 862.87, width: 125.55, height: 75.14)
-                break
-            case .icsVillage:
-                roomDimensions = CGRect(x: 127.94, y: 754.71, width: 44.09, height: 42.98)
-                break
-            case .iotVillage:
-                roomDimensions = CGRect(x: 127.94, y: 795.86, width: 44.09, height: 65.01)
-                break
-            case .lockpickVillage:
-                roomDimensions = CGRect(x: 169.07, y: 752.56, width: 64.13, height: 111.6)
-                break
-            case .packetCaptureVillage:
-                roomDimensions = CGRect(x: 495.8, y: 255.84, width: 150.22, height: 127.83)
-                break
-            case .socialEngineerVillage:
-                roomDimensions = CGRect(x: 786.54, y: 604.83, width: 64.33, height: 91.05)
-                break
-            case .tamperEvidentVillage:
-                roomDimensions = CGRect(x: 169, y: 861.57, width: 64.85, height: 75.14)
-                break
-            case .wirelessVillage:
-                roomDimensions = CGRect(x: 577, y: 932, width: 57, height: 61)
-                break
+            case .seminars:
+                roomDimensions = CGRect(x: 138.88, y: 348.61, width: 70.95, height: 73.48)
             case .unknown:
                 break
             }
@@ -85,34 +38,17 @@ class HTMapsViewController: UIViewController, UIScrollViewDelegate {
         automaticallyAdjustsScrollViewInsets = false
     }
     
-    func configure(contentOffset : CGPoint, zoom : CGFloat, timeOfDay : TimeOfDay)
-    {
-        dayTimeSwitch.selectedSegmentIndex = timeOfDay == .day  ? 0 : 1
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         automaticallyAdjustsScrollViewInsets = false
         
         let dayFile = Bundle.main.url(forResource: "toorcon-19-floorplan", withExtension: "pdf")
-        let nightFile = Bundle.main.url(forResource: "toorcon-19-floorplan", withExtension: "pdf")
 
         dayMapView = ReaderContentView(frame: self.view.frame, fileURL: dayFile!, page: 0, password: "")
         view.addSubview(dayMapView!)
 
         dayMapView?.backgroundColor = UIColor.backgroundGray
-
-        nightMapView = ReaderContentView(frame: self.view.frame, fileURL: nightFile!, page: 0, password: "")
-        view.addSubview(nightMapView!)
-
-        nightMapView?.backgroundColor = UIColor.backgroundGray
-
-        dayMapView?.maximumZoomScale = 8
-        nightMapView?.maximumZoomScale = 8
-
-        mapTypeChanged(dayTimeSwitch)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -128,14 +64,6 @@ class HTMapsViewController: UIViewController, UIScrollViewDelegate {
         if let roomDimensions = roomDimensions, roomDimensions.width > 0, roomDimensions.height > 0 {
             zoomToLocation(roomDimensions)
         }
-
-        /*if let timeOfDay = timeOfDay {
-            dayTimeSwitch.selectedSegmentIndex = timeOfDay == .night ? 1 : 0
-        } else {
-            dayTimeSwitch.selectedSegmentIndex = TimeOfDay.timeOfDay(for: Date()) == .night ? 1 : 0
-        }
-
-        mapTypeChanged(dayTimeSwitch)*/
     }
 
     func zoomToLocation(_ roomDimensions: CGRect) {
@@ -163,30 +91,7 @@ class HTMapsViewController: UIViewController, UIScrollViewDelegate {
         let roomCenter = CGPoint(x: roomCorner.x + (roomSize.width / 2), y: roomCorner.y + (roomSize.height / 2))
 
         dayMapView?.contentOffset = CGPoint(x: roomCenter.x - (size.width/2), y: roomCenter.y - (size.height/2))
-        nightMapView?.contentOffset = CGPoint(x: roomCenter.x - (size.width/2), y: roomCenter.y - (size.height/2))
 
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let setterView = scrollView == dayMapView ? nightMapView : dayMapView
-        let getterView = scrollView
-
-        setterView?.zoomScale = getterView.zoomScale
-        setterView?.contentOffset = getterView.contentOffset
-    }
-    
-    @IBAction func mapTypeChanged(_ sender: UISegmentedControl) {
-        let isDay = sender.selectedSegmentIndex == 0
-        let isNight = !isDay
-
-        dayMapView?.isHidden = isNight
-        nightMapView?.isHidden = isDay
-
-        dayMapView?.isUserInteractionEnabled = isDay
-        nightMapView?.isUserInteractionEnabled = isNight
-
-        nightMapView?.mapScrollViewDelegate = isNight ? self : nil
-        dayMapView?.mapScrollViewDelegate = isDay ? self : nil
     }
     
     func doneButtonPressed() {
--- hackertracker/MapLocationView.swift
+++ hackertracker/MapLocationView.swift
@@ -11,73 +11,26 @@ import UIKit
 public enum Location
 {
     case unknown
-    case track_101
-    case track1_101
-    case track2_101
+    case track1
     case track2
-    case track3
-    case track4
-    case trevi
-    case capri
-    case modena
-    case bioHackingVillage
-    case cryptoAndPrivacyVillage
-    case hardwareHackingVillage
-    case icsVillage
-    case iotVillage
-    case lockpickVillage
-    case packetCaptureVillage
-    case socialEngineerVillage
-    case tamperEvidentVillage
-    case wirelessVillage
+    case seminars
     
     public static func valueFromString(_ value : String) -> Location
     {
         switch value.lowercased() {
-        case "101 track 1":
-            return .track1_101
-        case "101 track 2":
-            return .track2_101
-        case "101 track":
-            return .track_101
-        case "track 3":
-            return .track3
-        case "track 4":
-            return .track4
-        case "trevi room":
-            return .trevi
-        case "capri room":
-            return .capri
-        case "modena room":
-            return .modena
+        case "track 1":
+            return .track1
         case "track 2":
             return .track2
-        case "BIO Hacking Village":
-            return .bioHackingVillage
-        case "Crypto and Privacy Village":
-            return .cryptoAndPrivacyVillage
-        case "Hardware Hacking Village":
-            return .hardwareHackingVillage
-        case "ICS Village":
-            return .icsVillage
-        case "IoT Village":
-            return .iotVillage
-        case "Lockpick Village":
-            return .lockpickVillage
-        case "Packet Capture Village":
-            return .packetCaptureVillage
-        case "Social-Engineer Village":
-            return .socialEngineerVillage
-        case "Tamper Evident Village":
-            return .tamperEvidentVillage
-        case "Wireless Village":
-            return .wirelessVillage
+        case "seminars":
+            return .seminars
         default:
             print("unkown location: \(value)")
             return .unknown
         }
     }
 }
+
 
 enum TimeOfDay
 {
@@ -124,73 +77,17 @@ class MapLocationView: UIView, UIWebViewDelegate, UIScrollViewDelegate {
         didSet
         {
             switch currentLocation {
-            case .track_101, .track1_101:
-                mapZoomLevel = 5.46535710851014
-                mapOffset = CGPoint(x: 1108.0, y: 1091.0)
+            case .track1:
+                mapZoomLevel = 8.32341261928717
+                mapOffset = CGPoint(x: 334.5, y: 641.5)
                 break
-            case .track2, .track2_101:
-                mapZoomLevel = 5.04600641912036
-                mapOffset = CGPoint(x: 936.333333333333, y: 352)
+            case .track2:
+                mapZoomLevel = 10.2361423152965
+                mapOffset = CGPoint(x: 1107.0, y: 756.5)
                 break
-            case .track3:
-                mapZoomLevel = 6.47638274448022
-                mapOffset = CGPoint(x: 281, y: 586.333333333333)
-                break
-            case .track4:
-                mapZoomLevel = 6.47638274448022
-                mapOffset = CGPoint(x: 441.666666666667, y: 586.333333333333)
-                break
-            case .trevi:
-                mapZoomLevel = 30.5747421643601
-                mapOffset = CGPoint(x: 4134.66666666667, y: 4805.66666666667)
-                break
-            case .modena:
-                mapZoomLevel = 30.5747421643601
-                mapOffset = CGPoint(x: 4086.66666666667, y: 5467.33333333333)
-                break
-            case .capri:
-                mapZoomLevel = 25.7730198429929
-                mapOffset = CGPoint(x: 3129.0, y: 6295.0)
-                break
-            case .bioHackingVillage:
-                mapZoomLevel = 15.5319305068955
-                mapOffset = CGPoint(x: 1824.0, y: 2302.0)
-                break
-            case .cryptoAndPrivacyVillage:
-                mapZoomLevel = 11.5321488259658
-                mapOffset = CGPoint(x: 1756.66666666667, y: 2700.33333333333)
-                break
-            case .hardwareHackingVillage:
-                mapZoomLevel = 5.99026466659583
-                mapOffset = CGPoint(x: 295.666666666667, y: 1173.66666666667)
-                break
-            case .icsVillage:
-                mapZoomLevel = 13.9902143693864
-                mapOffset = CGPoint(x: 392.666666666667, y: 2656.66666666667)
-                break
-            case .iotVillage:
-                mapZoomLevel = 13.9902143693864
-                mapOffset = CGPoint(x: 392.666666666667, y: 2838.33333333333)
-                break
-            case .lockpickVillage:
-                mapZoomLevel = 7.55686887825483
-                mapOffset = CGPoint(x: 240.0, y: 1466.0)
-                break
-            case .packetCaptureVillage:
-                mapZoomLevel = 4.78024461014065
-                mapOffset = CGPoint(x: 542.333333333333, y: 246.666666666667)
-                break
-            case .socialEngineerVillage:
-                mapZoomLevel = 9.11508857992245
-                mapOffset = CGPoint(x: 1729.66666666667, y: 1392.0)
-                break
-            case .tamperEvidentVillage:
-                mapZoomLevel = 7.15414767702267
-                mapOffset = CGPoint(x: 257.333333333333, y: 1487.33333333333)
-                break
-            case .wirelessVillage:
-                mapZoomLevel = 11.5321488259658
-                mapOffset = CGPoint(x: 1615.66666666667, y: 2700.33333333333)
+            case .seminars:
+                mapZoomLevel = 10.2361423152965
+                mapOffset = CGPoint(x: 502.0, y: 1329.0)
                 break
             default:
                 mapOffset = .zero
@@ -219,6 +116,8 @@ class MapLocationView: UIView, UIWebViewDelegate, UIScrollViewDelegate {
         
         setup()
     }
+    
+    
     
     func setup()
     {
