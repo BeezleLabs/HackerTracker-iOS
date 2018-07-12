a9ee83d10743b366d4efe3da8e56e8fc1f4cf97f
Seth Law <seth.w.law@gmail.com>
2018-07-12 10:24:18 -0600

    Revert "Revert "add FAQ table view controller""
    This reverts commit 10a0b85d1dad5263b7debbabad60a5aa7b31680a.

--- hackertracker/HTFAQTableViewController.swift
+++ hackertracker/HTFAQTableViewController.swift
@@ -1,23 +1,22 @@
 //
-//  HTConferenceTableViewController.swift
+//  HTFAQTableViewController.swift
 //  hackertracker
 //
-//  Created by Seth Law on 7/8/18.
+//  Created by Seth Law on 7/12/18.
 //  Copyright © 2018 Beezle Labs. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class HTConferenceTableViewController: UITableViewController {
-    
-    var conferences: [Conference] = []
+class HTFAQTableViewController: UITableViewController {
+
+    var faqs: [FAQ] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        NSLog("loading conferences")
-        self.loadConferences()
+        self.loadFAQs()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -33,42 +32,53 @@ class HTConferenceTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return conferences.count
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 68
+        return faqs.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "conferenceCell", for: indexPath) as! ConferenceCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "faqCell", for: indexPath)
 
-        var c: Conference
-        
-        c = self.conferences[indexPath.row]
-        //NSLog("setting row \(indexPath.row) to conference \(c.name)")
-        cell.setConference(conference: c)
+        cell.textLabel?.text = self.faqs[indexPath.row].question!
 
         return cell
     }
     
-    func loadConferences() {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let fr = NSFetchRequest<NSFetchRequestResult>(entityName:"Conference")
-        fr.sortDescriptors = [NSSortDescriptor(key: "start_date", ascending: true)]
-        fr.returnsObjectsAsFaults = false
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.left
         
-        conferences = try! getContext().fetch(fr) as! [Conference]
+        let body = "Q: \(self.faqs[indexPath.row].question!)\n\nA: \(self.faqs[indexPath.row].answer!)"
+        
+        let messageText = NSMutableAttributedString(
+            string: body,
+            attributes: [
+                NSAttributedStringKey.paragraphStyle: paragraphStyle,
+                NSAttributedStringKey.font: UIFont(name: "Larsseit", size: 14),
+                NSAttributedStringKey.foregroundColor : UIColor.black
+            ]
+        )
+        
+        let popup : UIAlertController = UIAlertController(title: "FAQ", message:"", preferredStyle: UIAlertControllerStyle.alert)
+        popup.setValue(messageText, forKey: "attributedMessage")
+        
+        let doneItem : UIAlertAction = UIAlertAction(title: "Done", style: UIAlertActionStyle.default, handler: nil)
+        popup.addAction(doneItem)
+        
+        self.present(popup, animated: true, completion: nil)
+        //self.present(popup, animated: true) {        }
+        
     }
     
-
+    func alertControllerBackgroundTapped()
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -95,6 +105,15 @@ class HTConferenceTableViewController: UITableViewController {
 
     }
     */
+    
+    func loadFAQs() {
+        
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName:"FAQ")
+        fr.sortDescriptors = [NSSortDescriptor(key: "updated_at", ascending: false)]
+        fr.returnsObjectsAsFaults = false
+        
+        faqs = try! getContext().fetch(fr) as! [FAQ]
+    }
 
     /*
     // Override to support conditional rearranging of the table view.
