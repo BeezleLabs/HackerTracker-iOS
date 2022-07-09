28a7c25d15b215bac723f174372d2da085368de1
Seth Law <seth.w.law@gmail.com>
2022-07-09 16:55:53 -0600

    Now with selectable buttons

---
+++ hackertracker/Views/CountDownCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountDownCell.swift
+//  hackertracker
+//
+//  Created by Seth W Law on 6/2/22.
+//  Copyright © 2022 Beezle Labs. All rights reserved.
+//
+
+import UIKit
+
+class CountDownCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
